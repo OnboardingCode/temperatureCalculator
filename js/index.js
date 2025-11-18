17466c432c7019690ad1cf23ffdd4ef5acad46bb
TenshiHimSelf <jrcontreras446@gmail.com>
2025-11-17 19:56:19 -0600

    added global values on the js file

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const tempInput = document.getElementById("textBox"); //User Temp value to read
+const toFahrenheit = document.getElementById("toFahrenheit"); //temp value to from C to F.
+const toFCelsius = document.getElementById("toFCelsius"); //temp value to from F to C.
+const submitBTN = document.getElementById("submit");//Submit acction BTN, must be on a Function.
+const result = document.getElementById("result"); //the result that will bi displayed on screen.
+let tempOutput; // Internal value for the result, this value stay on the code, and not displayed.
